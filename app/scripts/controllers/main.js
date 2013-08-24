213077459fe40467840c5be98b68c246da785a6a
Maciej Jankowski <maciej.artur.jankowski@gmail.com>
2013-08-24 11:20:50 +0200

    yo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bethechangeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
